06a378616ae9ccaf97c44f09e04b8b11e0196fa0
Rishitjain4874 <rishitjain4874@gmail.com>
2024-10-18 15:56:38 +0530

    responses from postman registered in console

--- src/middleware/authmiddleware.ts
+++ src/middleware/authmiddleware.ts
@@ -1,1 +1,21 @@
 const jwt = require('jsonwebtoken');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const requireAuth = (req: any, res: any, next: any) => {
+    const token = req.cookies.jwt;
+    if (token) {
+        jwt.verify(token, `${process.env.JWT_SECRET}`, (err: any , decodedToken: object | undefined) => {
+            if (err) {
+                console.log(err.message);
+                res.redirect('/admin/login');
+            } else {
+                console.log(decodedToken);
+                next();
+            }
+        });
+    } else {
+        res.redirect('/admin/login');
+    }
+}
