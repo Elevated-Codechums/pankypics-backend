ff5801a200f00686ed7e3a803a08a75c42ac3a88
Rishitjain4874 <rishitjain4874@gmail.com>
2024-10-20 01:56:25 +0530

    jwt returning tokens

--- src/middleware/authmiddleware.ts
+++ src/middleware/authmiddleware.ts
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
-
 const dotenv = require('dotenv');
+import poolDB from "../config/database.js";
 dotenv.config();
 
 const requireAuth = (req: any, res: any, next: any) => {
@@ -19,3 +19,22 @@ const requireAuth = (req: any, res: any, next: any) => {
         res.redirect('/admin/login');
     }
 }
+const checkAdmin = (req: any, res: any, next: any) => {
+    const token = req.cookies.jwt;
+    if (token) {
+        jwt.verify(token, `${process.env.JWT_SECRET}`, async (err: any, decodedToken: object | undefined) => {
+            if (err) {
+                console.log(err.message);
+                res.locals.admin = null;
+                next();
+            } else {
+                console.log(decodedToken);
+                let admin = await poolDB.query("SELECT admin_name, admin_email, password FROM admin where admin_id = $1", [decodedToken]);
+                res.locals.admin = admin.rows[0];
+                next();
+            }
+        });
+    } else {
+        res.locals.admin = null;
+        next();
+    }};
--- src/routers/authrouters.ts
+++ src/routers/authrouters.ts
@@ -7,4 +7,4 @@ const authRouter = express.Router();
 authRouter.get("/login", authcontrollers.login_get);
 authRouter.post("/login", authcontrollers.login_post);
 
-export default authRouter;
+export default authRouter; 
