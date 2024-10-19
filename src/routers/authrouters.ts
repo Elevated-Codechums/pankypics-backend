b1463d185f4881ef8c80a6221e667441c434494b
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2024-10-20 05:12:29 +0530

    file names changed

--- src/routers/authrouters.ts
+++ src/routers/authrouters.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import * as authcontrollers from "../controllers/authcontrollers.js";
+import * as authControllers from "../controllers/authControllers.js";
 
 
 const authRouter = express.Router();
 
-authRouter.get("/login", authcontrollers.login_get);
-authRouter.post("/login", authcontrollers.login_post);
+authRouter.get("/login", authControllers.login_get);
+authRouter.post("/login", authControllers.login_post);
 
 export default authRouter; 
